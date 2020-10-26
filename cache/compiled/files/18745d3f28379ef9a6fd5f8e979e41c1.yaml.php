7ab2cf2dc62e7dea6b15bf52143f99b4f15d1d27
Pierre Beuselinck <pierre.beuselinck@agencedig.com>
2020-10-26 21:46:37 +0100

    Update

---
+++ cache/compiled/files/18745d3f28379ef9a6fd5f8e979e41c1.yaml.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/xampp/htdocs/grav-accrobranche/system/config/streams.yaml',
+    'modified' => 1603264544,
+    'data' => [
+        'schemes' => [
+            'image' => [
+                'type' => 'Stream',
+                'paths' => [
+                    0 => 'user://images',
+                    1 => 'system://images'
+                ]
+            ],
+            'page' => [
+                'type' => 'ReadOnlyStream',
+                'paths' => [
+                    0 => 'user://pages'
+                ]
+            ],
+            'account' => [
+                'type' => 'ReadOnlyStream',
+                'paths' => [
+                    0 => 'user://accounts'
+                ]
+            ]
+        ]
+    ]
+];
